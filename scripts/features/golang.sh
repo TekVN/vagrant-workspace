171f5ee10348ab314073bf35e6e687f1761783a9
DNT <ducconit@gmail.com>
2024-02-22 09:31:39 +0700

    feat: Update golang version to 1.21.4

--- scripts/features/golang.sh
+++ scripts/features/golang.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 if [ -f ~/.features/wsl_user_name ]; then
-  WSL_USER_NAME="$(cat ~/.features/wsl_user_name)"
-  WSL_USER_GROUP="$(cat ~/.features/wsl_user_group)"
+    WSL_USER_NAME="$(cat ~/.features/wsl_user_name)"
+    WSL_USER_GROUP="$(cat ~/.features/wsl_user_group)"
 else
-  WSL_USER_NAME=vagrant
-  WSL_USER_GROUP=vagrant
+    WSL_USER_NAME=vagrant
+    WSL_USER_GROUP=vagrant
 fi
 
 export DEBIAN_FRONTEND=noninteractive
 
 if [ -f /home/$WSL_USER_NAME/.features/golang ]; then
-  echo "Golang already installed."
-  exit 0
+    echo "Golang already installed."
+    exit 0
 fi
 
 touch /home/$WSL_USER_NAME/.features/golang
@@ -21,11 +21,12 @@ chown -Rf $WSL_USER_NAME:$WSL_USER_GROUP /home/$WSL_USER_NAME/.features
 ARCH=$(arch)
 
 # Install Golang
-golangVersion="1.21.2"
 if [[ "$ARCH" == "aarch64" ]]; then
-  wget https://dl.google.com/go/go${golangVersion}.linux-arm64.tar.gz -O golang.tar.gz
+    GOLANG_LATEST_STABLE_VERSION=$(curl https://go.dev/dl/?mode=json | grep -o 'go.*.linux-arm64.tar.gz' | head -n 1 | tr -d '\r\n')
+    wget https://dl.google.com/go/${GOLANG_LATEST_STABLE_VERSION} -O golang.tar.gz
 else
-  wget https://dl.google.com/go/go${golangVersion}.linux-amd64.tar.gz -O golang.tar.gz
+    GOLANG_LATEST_STABLE_VERSION=$(curl https://go.dev/dl/?mode=json | grep -o 'go.*.linux-amd64.tar.gz' | head -n 1 | tr -d '\r\n')
+    wget https://dl.google.com/go/${GOLANG_LATEST_STABLE_VERSION} -O golang.tar.gz
 fi
 
 tar -C /usr/local -xzf golang.tar.gz go
