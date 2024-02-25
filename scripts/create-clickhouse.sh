4d86b9af29c664c0387cc30c36c5453bde761bcb
DNT <ducconit@gmail.com>
2024-02-25 15:25:23 +0700

    fix: add username workspace

--- scripts/create-clickhouse.sh
+++ scripts/create-clickhouse.sh
@@ -5,7 +5,7 @@ DB=$1
 clickhouse=$(ps ax | grep clickhouse-server | wc -l)
 
 if [ "$clickhouse" -gt 1 ]; then
-    clickhouse-client --port 9003 --password secret --query "CREATE DATABASE IF NOT EXISTS \`$DB\`"
+    clickhouse-client --port 9003 --password secret --user workspace --query "CREATE DATABASE IF NOT EXISTS \`$DB\`"
 else
     # Skip Creating database
     echo "We didn't find Clickhouse, skipping \`$DB\` creation"
