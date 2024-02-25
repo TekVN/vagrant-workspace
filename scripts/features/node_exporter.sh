65202f3ff52f172a1f2caf9f7841973fad860275
DNT <ducconit@gmail.com>
2024-02-25 22:04:40 +0700

    chore: disabled progress wget node_exporter

--- scripts/features/node_exporter.sh
+++ scripts/features/node_exporter.sh
@@ -17,7 +17,7 @@ fi
 
 # Install exporter
 cd /opt
-wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+wget -q https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
 tar -xf node_exporter-1.7.0.linux-amd64.tar.gz
 mv node_exporter-1.7.0.linux-amd64 node_exporter
 rm node_exporter-1.7.0.linux-amd64.tar.gz
